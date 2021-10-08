fbae24852ad958dca2de94c89cec0166525c9e0e
oladeleseyi <toothy61@gmail.com>
2021-10-08 23:26:43 +0100

    Cleaning The starter Route

--- handler.js
+++ handler.js
@@ -9,18 +9,8 @@ export const hello = async (event, context) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: `Go Serverless v2.0! ${await message({
-        time: 1,
-        copy: "Your function executed successfully!",
-      })}`,
-      happy,
+      message: "Success",
+      data: happy.getPrices[0],
     }),
   };
 };
-
-const message = ({ time, ...rest }) =>
-  new Promise((resolve, reject) =>
-    setTimeout(() => {
-      resolve(`${rest.copy} (with a delay)`);
-    }, time * 1000)
-  );
